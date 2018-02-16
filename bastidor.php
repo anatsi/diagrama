92f1bff96a3843558fe047f7cffc543713eb5b72
Ana Asins <aasins@tsiberia.es>
2018-02-16 15:46:14 +0100

    pattern puesto otra vez

--- bastidor.php
+++ bastidor.php
@@ -50,7 +50,7 @@ if (isset($_POST['origen']) || isset($_POST['otrosOrigenes'])) {
       <ul>
         <li>
           <label for="Bastidor" id="titulo">BASTIDOR</label>
-          <input type="text" name="bastidor" autofocus required/>
+          <input type="text" name="bastidor" autofocus required pattern="([W][F][a-zA-Z0-9]{8}[A-Z]{2}[0-9]{5})"/>
         </li>
       </ul>
       <?php
